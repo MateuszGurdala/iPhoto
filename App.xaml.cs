1097e9de2e3041f0d5b40ad0e277edacda12e55e
Krystian Grela <krystian12340@gmail.com>
2022-04-11 00:17:13 +0200

    added navigation service base (commented not implemented)

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,22 @@
 ﻿using System.Windows;
+using iPhoto.UtilityClasses;
 using iPhoto.ViewModels;
 
 namespace iPhoto
 {
     public partial class App : Application
     {
+        /*private readonly NavigationStore _navigationStore;
+        public App()
+        {
+            _navigationStore = new NavigationStore();
+        }*/
         protected override void OnStartup(StartupEventArgs e)
         {
+            //_navigationStore.CurrentViewModel = null;
             MainWindow = new MainWindow();
             MainWindow.DataContext = new MainWindowViewModel(MainWindow);
+            // MainWindow.DataContext = new MainWindowViewModel(_navigationStore);
             MainWindow.Show();
             base.OnStartup(e);
         }
