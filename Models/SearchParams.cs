57473bcefe9dc35f3851e67316c9e5791f3682fa
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2022-04-29 15:16:07 +0200

    Added custom date picker (need further development, but it kinda works)

--- Models/SearchParams.cs
+++ Models/SearchParams.cs
@@ -17,8 +17,10 @@ namespace iPhoto.Models
             _title = searchOptions.Title.ContentTextBox.Text;
             _photoAlbum = searchOptions.Album.Text;
             _tags = searchOptions.Tags.ContentTextBox.Text;
-            _startDate = searchOptions.StartDate.ContentTextBox.Text;
-            _endDate = searchOptions.EndDate.ContentTextBox.Text;
+            //_startDate = searchOptions.StartDate.ContentTextBox.Text;
+            _startDate = searchOptions.StartDate.Text;
+            //_endDate = searchOptions.EndDate.ContentTextBox.Text;
+            _endDate = searchOptions.EndDate.Text;
             _location = searchOptions.Location.ContentTextBox.Text;
 
             CheckForNull();
@@ -28,8 +30,8 @@ namespace iPhoto.Models
             _title = (_title[0] == '*') ? null : _title;
             _photoAlbum = (_photoAlbum[0] == '*') ? null : _photoAlbum;
             _tags = (_tags[0] == '*') ? null : _tags;
-            _startDate = (_startDate[0] == '*') ? null : _startDate;
-            _endDate = (_endDate[0] == '*') ? null : _endDate;
+            _startDate = (_startDate == "") ? null : _startDate;
+            _endDate = (_endDate == "") ? null : _endDate;
             _location = (_location[0] == '*') ? null : _location;
         }
         public string? GetTitleParam()
